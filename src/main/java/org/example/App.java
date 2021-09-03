009ae6de94210618b7b5e4ff2412affd6647851c
Brandon Terry <brandon2024@knights.ucf.edu>
2021-09-03 11:15:48 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,45 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Brandon Terry
+ */
+package org.example;
+import java.util.Scanner;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        int month;
+        Scanner input = new Scanner(System.in);
+        System.out.print( "Please enter the number of the month: " );
+        month = input.nextInt();
+
+        switch (month)
+        {
+            case 1: System.out.println("The name of the month is January.");
+                break;
+            case 2: System.out.println("The name of the month is February.");
+                break;
+            case 3: System.out.println("The name of the month is March.");
+                break;
+            case 4: System.out.println("The name of the month is April.");
+                break;
+            case 5: System.out.println("The name of the month is May.");
+                break;
+            case 6: System.out.println("The name of the month is June.");
+                break;
+            case 7: System.out.println("The name of the month is July.");
+                break;
+            case 8: System.out.println("The name of the month is August.");
+                break;
+            case 9: System.out.println("The name of the month is September.");
+                break;
+            case 10: System.out.println("The name of the month is October.");
+                break;
+            case 11: System.out.println("The name of the month is November.");
+                break;
+            case 12: System.out.println("The name of the month is December.");
+                break;
+        }
+    }
+}
