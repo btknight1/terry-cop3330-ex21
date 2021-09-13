0829f05e0c579e7d888fae2d56bdd31aac531973
Brandon Terry <brandon2024@knights.ucf.edu>
2021-09-13 17:53:41 -0400

    Updated 9/13/21.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -16,29 +16,44 @@ public class App
 
         switch (month)
         {
-            case 1: System.out.println("The name of the month is January.");
+            case 1:
+                System.out.println("The name of the month is January.");
                 break;
-            case 2: System.out.println("The name of the month is February.");
+            case 2:
+                System.out.println("The name of the month is February.");
                 break;
-            case 3: System.out.println("The name of the month is March.");
+            case 3:
+                System.out.println("The name of the month is March.");
                 break;
-            case 4: System.out.println("The name of the month is April.");
+            case 4:
+                System.out.println("The name of the month is April.");
                 break;
-            case 5: System.out.println("The name of the month is May.");
+            case 5:
+                System.out.println("The name of the month is May.");
                 break;
-            case 6: System.out.println("The name of the month is June.");
+            case 6:
+                System.out.println("The name of the month is June.");
                 break;
-            case 7: System.out.println("The name of the month is July.");
+            case 7:
+                System.out.println("The name of the month is July.");
                 break;
-            case 8: System.out.println("The name of the month is August.");
+            case 8:
+                System.out.println("The name of the month is August.");
                 break;
-            case 9: System.out.println("The name of the month is September.");
+            case 9:
+                System.out.println("The name of the month is September.");
                 break;
-            case 10: System.out.println("The name of the month is October.");
+            case 10:
+                System.out.println("The name of the month is October.");
                 break;
-            case 11: System.out.println("The name of the month is November.");
+            case 11:
+                System.out.println("The name of the month is November.");
                 break;
-            case 12: System.out.println("The name of the month is December.");
+            case 12:
+                System.out.println("The name of the month is December.");
+                break;
+            default: //For any value outside that range, display an appropriate error message.
+                System.out.println("Not a month!");
                 break;
         }
     }
